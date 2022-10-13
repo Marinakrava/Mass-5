9a81bb2cc6ebc3ca26d115f1836a021da6bddf99
yxa.kravchenko@yandex.ru <yxa.kravchenko@yandex.ru>
2022-10-13 22:58:30 +0300

    Добавьте файлы проекта.

---
+++ Mass 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mass_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = ((i % 2 != 0) && (j % 2 == 0) || (i % 2 == 0) && (j % 2 != 0)) ? 0 : 1;// если четная строка и нечетный столбец, то в ячейке будет 0,
+                                                                                                         // а если нечетная строка и четный столбец, то будет 1
+
+                    Console.Write( " {0}", array[i,j]);                 
+                }
+                Console.WriteLine();
+
+            }
+         
+            Console.ReadKey();
+
+        }
+    }
+}
